f4139267509a730b5bfe586596c24eb12e54a1b9
rcrespo9 <rudycrespo@hushmail.com>
2013-09-27 01:31:31 -0400

    Remove username column from users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,
-  :username, :full_name, :location, :about, :avatar
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :full_name, :location, :about, :avatar
   validates :full_name, presence: true
   # attr_accessible :title, :body
   has_many :photo_inspireds
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130927033642) do
+ActiveRecord::Schema.define(:version => 20130927052726) do
 
   create_table "music_inspireds", :force => true do |t|
     t.string   "song_title"
@@ -47,7 +47,6 @@ ActiveRecord::Schema.define(:version => 20130927033642) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "username"
     t.string   "location"
     t.text     "about"
     t.string   "avatar_file_name"
